f4ce0b40935346aab7eb31ea9812b27d6823365c
Dexter Rautenbach <Dexter.Rautenbach@standardbank.co.za>
2018-04-05 21:30:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# Wifi Presence Alerter
+
+A bash based script to ping a list of devices and alert of the presence changes
+
+## Getting Started
+
+### Prerequisites
+
+The following packages should be installed already, but if they aren't, then run the following:
+
+```
+sudo apt-get install vim
+```
+
+Tmux will also be used to run the script in a session that won't be closed when disconnecting.
+```
+sudo apt-get install tmux
+```
+
+### Global variables 
+
+You will need to set the following global variable
+```
+WIFI_PRESENCE_ALERTER_ROOT
+```
+To set this, run the following command:
+```
+sudo vim /etc/environment
+```
+and add the following
+```
+export WIFI_PRESENCE_ALERTER_ROOT={THE FULL DIRECTORY WHERE YOUR "ping_wifi_ip_address.sh" IS LOCATED}
+```
+
+### Configuration
+
+The following configuration files are required:
+
+```
+device_list.lst
+telegram_bot_properties.props
+```
+
+The contents for device_list.lst should contain the following. Multiple configurations are supported:
+
+```
+DeviceOwner=ip_address
+DeviceOwner_2=ip_address_2
+```
+
+The contents for telegram_bot_properties.props should contain the following. 
+
+If multiple entries are made, the alert will be sent to each config.
+
+```
+BOT_API_KEY|CHAT_ID
+BOT_API_KEY_2|CHAT_ID_2
+```
+
+### Bot Details
+
+Your api key for your bot can be retrieved from bot_father on telegram.
+
+To find your chat_id, do the following (Note: The following should be done ***without*** a responsive bot framework hosting your bot).
+1. Create a group with your bot, or a private chat if you are not intending on using the bot in a group.
+2. Send a inline message to the bot (A message starting with "/")
+3. Run the following command:
+```
+curl -i -X GET https://api.telegram.org/bot{BOT_API_KEY}/getUpdates
+```
+You will get a response similar to the following. The chat_id is included in the "chat" section:
+```
+{
+  "ok": true,
+  "result": {
+    "message_id": xxx,
+    "from": {
+      "id": xxx,
+      "is_bot": true,
+      "first_name": "xxx",
+      "username": "xxx"
+    },
+    "chat": {
+      "id": 12345, //This line contains the CHAT_ID
+      "title": "xxx",
+      "type": "xx",
+      "all_members_are_administrators": true
+    },
+    "date": xx,
+    "text": "xx"
+  }
+}
+```
+4. Use this value in the config file to send messages to this particular group/private chat.
+
+### Running it in a non-ending session
+You can start a new tmux session as follows:
+```
+tmux new -s1
+```
+
+Then run your script:
+```
+${WIFI_PRESENCE_ALERTER_ROOT}/ping_wifi_ip_address.sh
+```
+You can then detach from your session by pressing the following:
+```
+"CTRL+b" and then "d"
+```
+
+To re-attach to you sessionso that you can see your script run etc, run the following
+```
+tmux attach-session -t1
+```
+
+Remember to detach from your session, and then you can safely close your session.
---
+++ ping_wifi_ip_address.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+# 	Author: 	Dex
+# 	Date:		2018/03/30
+# 	Purpose:	This script is used to ping devices on the wifi network and send alerts when they either leave or arrive
+
+base_ip=192.168.1.0
+root_dir=${WIFI_PRESENCE_ALERTER_ROOT}
+state_file=${root_dir}/state
+current_run_state=${root_dir}/state_current_run
+bot_properties=${root_dir}/telegram_bot_properties.props
+device_list=${root_dir}/device_list.lst
+retry_count=5
+sleep_interval=60
+
+check_for_state(){
+	if [[ ! -f ${state_file} ]]
+	then
+		echo "State file missing, creating it"
+		touch ${state_file}
+	fi
+
+}
+
+check_if_bot_file_exists(){
+	if [[ ! -f ${bot_properties} ]]
+	then
+		echo "Bot key file is missing, alerting will not go to telegram..."
+		return 1
+	fi
+	#file exists, so return 0
+	return 0
+}
+
+send_alert(){
+	state=${1}
+	person=${2}
+
+	if [[ ${state} == 0 ]]
+	then
+		message="${person} is now at home"
+	else
+		message="${person} is no longer at home"
+	fi
+	
+	echo "${message}"
+
+        check_if_bot_file_exists
+        bot_key_avail=${?}
+
+	#This section is for the bot alerting, if config is available
+	if [[ ${bot_key_avail} == 0 ]]
+	then
+		while read props || [[ -n "$props" ]]; do
+			bot_key=`echo ${props} | cut -d"|" -f1`
+			chat_id=`echo ${props} | cut -d"|" -f2`
+			echo "Sending bot alert now"
+			send_bot_alert "${bot_key}" "${chat_id}" "${message}"
+		done < ${bot_properties}
+	fi
+}
+
+send_bot_alert(){
+	bot_key=${1}
+	chat_id=${2}
+	message=${3}
+
+	#curl -s -X POST https://api.telegram.org/bot${bot_key}/sendMessage -d text="${message}" -d chat_id="${chat_id}" >> /dev/null
+
+}
+
+compare_states(){
+	new_state=${1}
+	old_state=${2}
+	device=${3}
+	person=${4}
+
+	if [[ ! "${new_state}" == "${old_state}" ]]
+	then
+		set_state ${device} ${new_state} ${state_file} ${person}
+		echo "${person}|${new_state}" >> ${current_run_state}
+	fi
+}
+
+check_device_in_state(){
+	ip_address=${1}
+	ping_result=${2}
+	person=${3}
+
+	state_from_file=`grep "${person}|${ip_address}" ${state_file}`
+	exists=$?
+	if [[ ${exists} == 1 ]]
+	then
+		echo "${person}|${ip_address}=${ping_result}" >> ${state_file}
+	else
+		new_state=`echo ${state_from_file} | cut -d"=" -f2`
+		compare_states ${ping_result} ${new_state} ${ip_address} ${person}
+	fi
+}
+
+set_state(){
+	device=${1}
+	new_state=${2}
+	state_file=${3}
+	person=${4}
+
+	awk -v pat="^${person}|${device}=" -v value="${person}|${device}=${new_state}" '{ if ($0 ~ pat) print value; else print $0; }' $3 > $3.tmp
+	mv $3.tmp $3
+}
+
+ping_address(){
+	ip_address=${1}
+	count=0
+	until [[ ${count} -ge ${retry_count} ]]
+	do
+		count=$((count+1))
+		echo "Attempt ${count}/${retry_count}"
+		ping -c1 ${ip_address} > /dev/null
+		result=${?}
+		if [[ ${result} == 0 ]]
+		then
+			return 0
+		fi
+		sleep 5
+	done
+	return 1
+}
+
+check_if_alerting_required(){
+	if [[ -f ${current_run_state} ]]
+	then
+		#The file exists, meaning there were state changes
+		if [[ ! $(wc -l <  ${current_run_state}) -eq $(wc -l < ${device_list}) ]]
+		then
+			#The line count from state change file doesn't matches line count from device list, so the states didnt change together
+			while read -r state_change || [[ -n "$state_change" ]]; do
+				current_person=`echo ${state_change} | cut -d"|" -f1`
+				current_person_state=`echo ${state_change} | cut -d"|" -f2`
+				other_person_state=`cat ${state_file} | grep -v ${current_person} | cut -d"=" -f2`
+				if [[ ! "${other_person_state}" == "0"  ]]
+				then
+					#Other person is not home, so lets alert
+					send_alert "${current_person_state}" "${current_person}"
+				fi
+			done < ${current_run_state}
+		fi
+	fi
+}
+
+delete_state_file(){
+	file_to_delete=${1}
+	if [[ -f ${file_to_delete} ]]
+	then
+		rm ${file_to_delete}
+	fi
+}
+	
+process_device_file(){
+	delete_state_file ${current_run_state}
+	while read -r device || [[ -n "$device" ]]; do
+        	person=`echo ${device} | cut -d"=" -f1`
+        	ip_address=`echo ${device} | cut -d"=" -f2`
+	
+        	echo "Checking [${person}]'s device: ${ip_address}"
+        	ping_address ${ip_address}
+        	ping_result=${?}
+
+        	check_device_in_state ${ip_address} ${ping_result} ${person}
+	done < ${device_list}
+	
+	#Now we check if there is alerting required
+	check_if_alerting_required
+	delete_state_file ${current_run_state}
+}
+
+#First check if state file exists
+check_for_state
+
+#Loop indefinitely with sleeps inbetween
+while true
+do
+	echo `date`
+	process_device_file 
+	echo
+	sleep ${sleep_interval}
+done
+
